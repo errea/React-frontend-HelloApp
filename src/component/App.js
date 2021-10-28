8b01547ea04b38ae9a4d4ba2849c76e545ac7198
errea <erreakezy@gmail.com>
2021-10-28 16:48:17 +0100

    Modify App.js file

--- src/component/App.js
+++ src/component/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import Greeting from './Message';
+import Message from './Message';
 import store from '../redux/configureStore';
 
 const App = () => (
@@ -9,7 +9,7 @@ const App = () => (
     <Provider store={store}>
       <Router>
         <Switch>
-          <Route exact path="/" render={() => <Greeting />} />
+          <Route exact path="/" render={() => <Message />} />
         </Switch>
       </Router>
     </Provider>
